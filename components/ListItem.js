130724c0872e27e00c7e7335f779a3986c681a30
Takapon0407 <takapon_business@yahoo.co.jp>
2021-02-17 07:05:11 +0900

    カードの表示をコンポーネント化し、ListItemとして呼び出すように修正

---
+++ components/ListItem.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+});
+
+const ListItem = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.leftContainer}>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text numberOfLines={3} style={styles.text}>
+          あなたも今多分その尊敬ように対するもののうちに思いんです。もし近頃を指図顔は同時にその尊重なけれななどをいうているたにも用意なるですですて、なぜにはやるなたでます。先輩を受けでものはけっして十月にもっとませなませ。
+        </Text>
+        <Text style={styles.subText}>ReactNews</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ListItem;
