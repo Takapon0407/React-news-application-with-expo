efab71771c8f1ad085e4c0aa50a549979e8090e5
Takapon0407 <takapon_business@yahoo.co.jp>
2021-02-17 23:02:34 +0900

    APIで取得した記事を表示するように修正

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
 import dummyarticles from './dammies/dammies.json';
 import { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,16 +20,22 @@ export default function App() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setArticles(dummyarticles);
-    }, 2000);
-    return () => clearTimeout(timer);
+    fetchArticles();
   }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.log('error');
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={dummyarticles}
+        data={articles}
         renderItem={({ item }) => (
           <ListItem
             imageUrl={item.urlToImage}
