efc1ab2ed8ce9223e849b3e3bd5f48630ba7cf22
Takapon0407 <takapon_business@yahoo.co.jp>
2021-02-20 01:35:23 +0900

    記事一覧の取得時に表示されるように修正

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from '../components/ListItem';
 import { NEWS_API_KEY } from '@env';
 import axios from 'axios';
+import Loading from '../components/Loading';
 
 const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`;
 
@@ -15,18 +16,21 @@ const styles = StyleSheet.create({
 
 export default HomeScreen = ({ navigation }) => {
   const [articles, setArticles] = useState([]);
+  const [Loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchArticles();
   }, []);
 
   const fetchArticles = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(URL);
       setArticles(response.data.articles);
     } catch (error) {
       console.log('error');
     }
+    setLoading(false);
   };
 
   return (
@@ -43,6 +47,7 @@ export default HomeScreen = ({ navigation }) => {
         )}
         keyExtractor={(item, index) => index.toString()}
       />
+      {loading && <Loading />}
     </SafeAreaView>
   );
 };
